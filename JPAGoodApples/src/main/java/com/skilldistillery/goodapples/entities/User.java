838156ec6d136ddb60a91f3ec5a1b3df3177e2e5
Cody Comstock <mr.codysherwood@hotmail.com>
2023-10-24 18:38:29 -0500

    fixed GeneratedValue annotation in User entity

--- JPAGoodApples/src/main/java/com/skilldistillery/goodapples/entities/User.java
+++ JPAGoodApples/src/main/java/com/skilldistillery/goodapples/entities/User.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -15,7 +16,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 public class User {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	@Column(name="first_name")
