e958e4b1d7edf80bd9c40ddd7f86317d97613c90
Sawyer Harnedy <sawyer@brucebs-mbp-2.attlocal.net>
2023-10-24 10:37:14 -0600

    stub spring boot

---
+++ GoodApples/src/main/java/com/skilldistillery/goodapples/GoodApplesApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.goodapples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodApplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodApplesApplication.class, args);
+	}
+
+}
---
+++ GoodApples/src/test/java/com/skilldistillery/goodapples/GoodApplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.goodapples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodApplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
