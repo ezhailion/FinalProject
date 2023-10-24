d3378bb377ee769adf4d889e1ebd0b0d6a9a2002
Maggie Barton <maggiebarton2@gmail.com>
2023-10-24 16:49:26 -0600

    message and reflection entities without relationship mappings, tests passing

---
+++ JPAGoodApples/src/main/java/com/skilldistillery/goodapples/entities/Message.java
@@ -0,0 +1,105 @@
+package com.skilldistillery.goodapples.entities;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+public class Message {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String content;
+	
+	@Column(name="create_date")
+	@CreationTimestamp
+	private LocalDate createDate;
+	
+	@Column(name="last_update")
+	@UpdateTimestamp
+	private LocalDate lastUpdate;
+	
+	private Boolean enabled;
+	
+	//TODO: user and message relationships
+
+	public Message() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDate getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDate createDate) {
+		this.createDate = createDate;
+	}
+
+	public LocalDate getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(LocalDate lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Message other = (Message) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [id=" + id + ", content=" + content + ", createDate=" + createDate + ", lastUpdate="
+				+ lastUpdate + ", enabled=" + enabled + "]";
+	}
+	
+	
+
+}
---
+++ JPAGoodApples/src/test/java/com/skilldistillery/goodapples/entities/GenderTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.goodapples.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GenderTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Gender gender;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAGoodApples");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		gender = em.find(Gender.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		gender = null;
+	}
+
+	@Test
+	void test_basic_mapping() {
+		assertNotNull(gender);
+		assertEquals("unspecified", gender.getName());
+	}
+
+}
