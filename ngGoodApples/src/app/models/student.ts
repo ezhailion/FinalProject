f19635b978fdb94994d93f30d3b26a9eae8cad3d
Maggie Barton <maggiebarton2@gmail.com>
2023-10-26 15:45:31 -0600

    added whoami to student model, first and last names of students displaying in student grid

--- ngGoodApples/src/app/models/student.ts
+++ ngGoodApples/src/app/models/student.ts
@@ -4,16 +4,19 @@ export class Student {
   id: number;
   accommodations : string;
   nickname : string;
+  whoami : User;
 
   constructor(
     id: number = 0,
     accommodations : string = '',
     nickname : string = '',
+    whoami : User = new User ()
 
   ) {
     this.id = id;
     this.accommodations = accommodations;
     this.nickname = nickname;
+    this.whoami = whoami;
 
   }
 
