5a5c42cb095f444edebafb1da73bf1d49b60a984
Maggie Barton <maggiebarton2@gmail.com>
2023-10-28 13:33:30 -0600

    add and remove checkboxes workinggit add .git add .git add .git add .git add .

--- ngGoodApples/src/app/services/teacher.service.ts
+++ ngGoodApples/src/app/services/teacher.service.ts
@@ -5,6 +5,7 @@ import { Observable, catchError, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
+import { Report } from '../models/report';
 
 @Injectable({
   providedIn: 'root',
@@ -37,14 +38,58 @@ export class TeacherService {
       );
   }
   disableSelf(user: User) {
-    console.log("we are not getting here")
-    return this.http.delete<User>(this.url + 'api/users', this.getHttpOptions())
+    console.log('we are not getting here');
+    return this.http
+      .delete<User>(this.url + 'api/users', this.getHttpOptions())
       .pipe(
         catchError((err: any) => {
           console.log(err);
           return throwError(
             () =>
-              new Error('TeacherService.disable(): error disabling user: ' + err)
+              new Error(
+                'TeacherService.disable(): error disabling user: ' + err
+              )
+          );
+        })
+      );
+  }
+
+  //REPORT TO BEHAVIOR STUFF DOWN HERE
+  addBehaviorToReport(reportId: number, behaviorId: number) {
+    return this.http
+      .post<Report>(
+        this.url + 'api/reports/' + reportId + '/behaviors/' + behaviorId,
+        null, this.getHttpOptions()
+      )
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError(
+            () =>
+              new Error(
+                'TeacherService.addBehaviorToReport(): error adding behavior: ' +
+                  err
+              )
+          );
+        })
+      );
+  }
+
+  removeBehaviorFromReport(reportId: number, behaviorId: number) {
+    return this.http
+      .delete<Report>(
+        this.url + 'api/reports/' + reportId + '/behaviors/' + behaviorId,
+        this.getHttpOptions()
+      )
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError(
+            () =>
+              new Error(
+                'TeacherService.removeBehaviorFromReport(): error removing behavior: ' +
+                  err
+              )
           );
         })
       );
